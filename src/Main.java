dd9b3abb95874c8e145752d58cc4b1ffea6a87bc
Roman <romanze1706@gmail.com>
2022-11-29 11:06:16 +0300

    билет 6 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
+  public static void getNum (int [] num) {
+      for (int i = 2; i <= 6; i++) {
+          if (num[i] % 2 ==0) {
+              num[i] +=1;
+          }
+          System.out.println(num[i]);
+
+      }
+  }
+
     public static void main(String[] args) {
 
-    }
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        getNum(num);
+
+  }
 }
